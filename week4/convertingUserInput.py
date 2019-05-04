ce2d9e27dd9a3154f646672a7f1e56dcaddfb1e1
Vasu Goel <goelvasu0712@gmail.com>
2019-05-04 21:00:10 +0530

    Add convertingUserInput.py to week4/

---
+++ week4/convertingUserInput.py
@@ -0,0 +1,7 @@
+# Floor 0 in europe is Floor 1 in US
+
+# Program to convert user input Europe Floor number to US Floor number
+
+euroFloor = input('Europe floor?\n')
+usFloor = int(euroFloor) + 1
+print('US floor:', usFloor)
