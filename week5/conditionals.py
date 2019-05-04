88499d479ade9a0f04fac5f152d265eca3c82981
Vasu Goel <goelvasu0712@gmail.com>
2019-05-04 21:03:01 +0530

    Add conditionals.py to week5/

---
+++ week5/conditionals.py
@@ -0,0 +1,7 @@
+for i in range(5):
+    print(i)
+    if i > 3:
+        print(i, 'is greater than 3')
+        print('Last statement of if')
+    print('Last statement of for loop')
+print('End of the program after for loop')
