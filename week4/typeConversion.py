f3dd9bf1d333f345a49b2698f005593c8347551a
Vasu Goel <goelvasu0712@gmail.com>
2019-05-04 20:58:28 +0530

    Add typeConversions.py to week4/

---
+++ week4/typeConversion.py
@@ -0,0 +1,8 @@
+print(type(23.32))  # Prints the type of object passed into type()
+
+print(float(100) + 1)   # Prints 101.0
+
+print(int(float(10) + 5))   # Prints 15
+
+a = 50.5
+print("a =" , a, "has type", type(int(a)))     # Prints a = 50.5 has type <class 'int'>
