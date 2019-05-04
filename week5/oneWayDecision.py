837947212e0324c8ea1e7148df2076c2df2c13ad
Vasu Goel <goelvasu0712@gmail.com>
2019-05-04 21:01:33 +0530

    Add oneWayDecision.py to week5/

---
+++ week5/oneWayDecision.py
@@ -0,0 +1,15 @@
+# Unlike other programming languages, indenting plays a significant role in python
+
+x = 5
+print('Before 5')
+if x == 5:
+    print('Is 5')
+    print('Is Still 5')
+    print('Third 5')
+print('After 5')
+print('Before 6')
+if x == 6:
+    print('Is 6')
+    print('Is Still 6')
+    print('Third 6')
+print('After 6')
