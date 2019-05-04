1acd7d6d80baa44c1380494d486405f2a3ce464a
Vasu Goel <goelvasu0712@gmail.com>
2019-05-04 20:57:54 +0530

    Add numericExpressions.py to week4/

---
+++ week4/numericExpressions.py
@@ -0,0 +1,19 @@
+a = 2
+b = 3
+c = 4
+d = 5
+
+a = a + b
+b = a - b
+c = c * a
+d = c / d
+
+e = a ** d      # Raise to power
+f = e % b       # Remainder or modulo
+
+print("a =", a)
+print("b =", b)
+print("c =", c)
+print("d =", d)
+print("e =", e)
+print("f =", f)
