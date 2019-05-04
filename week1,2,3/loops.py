b036d84fd88258a7eb04fbfa4924dd9e07638713
Vasu Goel <goelvasu0712@gmail.com>
2019-05-04 20:55:48 +0530

    Add loops.py to week1,2,3/

---
+++ week1,2,3/loops.py
@@ -0,0 +1,8 @@
+n = 5
+while n > 0:
+    print(n)
+    n = n-1
+    # print('Blastoff')
+    # Typing print statement indented with other statements in while loops it also so we get Blastoff printed after every number
+print('Blastoff')
+# Above print statement is one level left of other statements in while i.e aligned with the while statement which is why it is ran after the while loop executes
